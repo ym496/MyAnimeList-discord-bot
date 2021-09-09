2298f4e7d1c181b3d097fcc5cf691ef4a04a6e5b
yogii <47711835+WildCyclotron@users.noreply.github.com>
2021-09-09 17:30:43 +0530

    Update userflags.py

--- cogs/userflags.py
+++ cogs/userflags.py
@@ -1,13 +1,14 @@
 from cogs.getjson import jikanjson
 
+"""To send embeds of lists of animes/mangas of the requested status."""
 
 async def userstatus(ctx,name,url,status,bigstatus,type,title):
   try:
-    userstatus = jikanjson(url)
+    user_status = jikanjson(url)
   except:
     await ctx.send(f"{name}'s list is not public :smiling_face_with_tear: ")
     return
-  user_list = userstatus[type]
+  user_list = user_status[type]
   if len(user_list)> 15:
     await bigstatus(ctx,user_list,title)
   elif len(user_list) == 0:
